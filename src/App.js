2a5b724ed76d974ba3e3c0acd038cea7798ff675
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-11 18:55:42 -0500

    add extra routes for app

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ const App = () => (
       Navbar
       <Routes>
         <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
