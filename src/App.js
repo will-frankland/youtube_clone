90b8ae11d5fd9c083d3fe44d5f6c629a75072d8d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-11 18:54:17 -0500

    add imports to App.js and setup first route to homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000'}}>
+      Navbar
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+)
 
 export default App
