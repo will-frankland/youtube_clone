2a38d8935faed7d774c52c54f038e4db85a0f032
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-12 03:11:22 -0500

    add imports to VideoCard component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
 const VideoCard = () => {
   return (
