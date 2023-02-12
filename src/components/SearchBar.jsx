477d65dba49dcd9f4bb6889bbe644c70d2cd81c4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-11 19:48:30 -0500

    add imports to Feed, divide page and add copyright detailing with styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Paper, IconButton } from '@mui/material';
-import { Search } from '@mui/icons-material';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
   return (
     <Paper
-      component='form'
+      component="form"
       onSubmit={() => {}}
       sx={{
         borderRadius: 20,
-        border : '1px solid #e3e3e3',
+        border: "1px solid #e3e3e3",
         pl: 2,
-        boxShadow: 'none',
-        mr: { sm: 5 }
+        boxShadow: "none",
+        mr: { sm: 5 },
       }}
     >
-      <input 
-        className='search-bar'
-        placeholder='Search...'
+      <input
+        className="search-bar"
+        placeholder="Search..."
         value=""
         onChange={() => {}}
-        />
-      <IconButton type='submit' sx={{ p: '10px', color: 'red'}}>
+      />
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
         <Search />
       </IconButton>
     </Paper>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
