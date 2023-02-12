f00d476bab928b76c59b4460a5bfe60d1fc1daac
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-12 02:34:06 -0500

    create fetchFromAPI file and add headers from Rapid API. Clean up and set key to be stored in process.env

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -25,7 +25,7 @@ const Feed = () => {
         <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color: 'white' }}>
           New <span style={{ color: "#fc1503" }}>videos</span>
         </Typography>
-        <Videos />
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   );
