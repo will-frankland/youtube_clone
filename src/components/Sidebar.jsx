5e80037ad053166e7c997d41263fc713ff583f17
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-12 02:20:41 -0500

    add styling to span icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,31 @@
 import { Stack } from "@mui/material";
 import { categories } from "../utils/constants";
 
-const selectedCategory = 'New';
-
+const selectedCategory = "New";
 
 const Sidebar = () => (
   <Stack
-  direction='row'
-  sx={{
-    overflowY: 'auto',
-    height: { sx: 'auto', md: '95%' },
-    flexDirection: { md: 'column' },
-  }}
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
   >
     {categories.map((catagory) => (
       <button
-      className="category-btn"
-      style={{
-        background: catagory.name === selectedCategory && '#Fc1503'}}>
-        <span>{catagory.icon}</span>
-        <span>{catagory.name}</span>
+        className="category-btn"
+        style={{
+          background: catagory.name === selectedCategory && "#Fc1503",
+          color: 'white'
+        }}
+        key={catagory.name}
+      >
+        <span style={{ color: catagory.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{catagory.icon}</span>
+        <span style={{ opacity: catagory.name === selectedCategory ? '1' : '0.8' }}>{catagory.name}</span>
       </button>
     ))}
   </Stack>
-)
+);
 
 export default Sidebar;
