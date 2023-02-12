88450c32a050a03c3484b5c30250de2b38afbfd8
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-12 02:44:49 -0500

    add useEffect and useState functions to Feed in order to utilize API and set the state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,17 +12,17 @@ const Sidebar = () => (
       flexDirection: { md: "column" },
     }}
   >
-    {categories.map((catagory) => (
+    {categories.map((category) => (
       <button
         className="category-btn"
         style={{
-          background: catagory.name === selectedCategory && "#Fc1503",
+          background: category.name === selectedCategory && "#Fc1503",
           color: 'white'
         }}
-        key={catagory.name}
+        key={category.name}
       >
-        <span style={{ color: catagory.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{catagory.icon}</span>
-        <span style={{ opacity: catagory.name === selectedCategory ? '1' : '0.8' }}>{catagory.name}</span>
+        <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }}>{category.icon}</span>
+        <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
       </button>
     ))}
   </Stack>
