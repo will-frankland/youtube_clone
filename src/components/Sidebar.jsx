98df400d20f6eb54ff376095adb7c58c22fd681d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-12 02:50:07 -0500

    add onClick handler to button and use function to setSelectedCategory to allow buttons to change in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,6 +13,7 @@ const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
     {categories.map((category) => (
       <button
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#Fc1503",
           color: 'white'
