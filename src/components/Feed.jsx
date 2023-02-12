338ff70a153668aba0bd2cd5baa22bff49398b21
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-12 02:24:19 -0500

    add new Box component with styling to show videos heading within Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import React, { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: 'row' }}}>
-      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
         <Sidebar />
-        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright 2023 WF Media
         </Typography>
       </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+          New <span style={{ color: "#fc1503" }}>videos</span>
+        </Typography>
+      </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
