66f3d451d5edb80ddb3e9c21584d87d694a7e399
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 13:55:31 -0500

    add useParams import and pass it searchTerm. Add searchTerm to API query

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=`).then((data) =>
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
       setVideos(data.items)
     );
-  }, []);
+  }, [searchTerm]);
 
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
