da2867fa11eaa375ffb333ee9a977ace524bb0b8
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 15:30:48 -0500

    add if statement to display 'Loading...' if no video snippet found in API query

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,33 +1,54 @@
-import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import ReactPlayer from 'react-player';
-import { Typography, Box, Stack } from '@mui/material';
-import { CheckCircle } from '@mui/icons-material';
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-import { Video } from './';
-import { fetchFromAPI } from '../utils/fetchFromAPI';
-
+import { Video } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
-      .then((data) => setVideoDetail(data.items[0]));
-  }, [id])
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    )
+  }, [id]);
+
+  if(!videoDetail?.snippet) return 'Loading...';
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount }
+  } = videoDetail;
 
   return (
-    <Box minHeight='95vh'>
-      <Stack direction={{ xs: 'column', md: 'row '}}>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row " }}>
         <Box flex={1}>
-          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack direction='row' justifyContent='space-between' sx={{ color: '#fff'}} py={1} px={2}>
+              <Link to={`/channel/${channelId}`}>
+                <Typography>
+                  {channelId}
+                </Typography>
+              </Link>
+            </Stack>
           </Box>
         </Box>
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default VideoDetail
+export default VideoDetail;
