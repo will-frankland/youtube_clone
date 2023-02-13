c4ca4bef5b7d3e1dc9a80b289c7535287d8ffdef
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 13:56:53 -0500

    add dnamic and static code to display 'Showing videos for {searchTerm} videos' when search results displayed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -17,7 +17,7 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search results for:  <span style={{ color: "#fc1503" }}>videos</span>
+        Search results for:  <span style={{ color: "#fc1503" }}>{searchTerm}</span> videos
       </Typography>
       <Videos videos={videos} />
     </Box>
