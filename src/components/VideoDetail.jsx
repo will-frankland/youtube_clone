bf656f1f806bfa329e825bbe8a2927d35430550f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 16:02:46 -0500

    move Box inside stack for recommended videos

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -14,17 +14,19 @@ const VideoDetail = () => {
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
-    setVideoDetail(data.items[0]));
+      setVideoDetail(data.items[0])
+    );
 
-    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}$type=video`)
-      .then((data) => setVideos(data.items));
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}$type=video`).then(
+      (data) => setVideos(data.items)
+    );
   }, [id]);
 
-  if(!videoDetail?.snippet) return 'Loading...';
+  if (!videoDetail?.snippet) return "Loading...";
 
   const {
     snippet: { title, channelId, channelTitle },
-    statistics: { viewCount, likeCount }
+    statistics: { viewCount, likeCount },
   } = videoDetail;
 
   return (
@@ -40,14 +42,25 @@ const VideoDetail = () => {
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
-            <Stack direction='row' justifyContent='space-between' sx={{ color: '#fff'}} py={1} px={2}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              py={1}
+              px={2}
+            >
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: 'subtitle1', md: 'h6' }} color='#fff'>
+                <Typography
+                  variant={{ sm: "subtitle1", md: "h6" }}
+                  color="#fff"
+                >
                   {channelId}
-                  <CheckCircle sx={{ fontSize: '12px', color: 'gray', ml: '5px' }} />
+                  <CheckCircle
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
                 </Typography>
               </Link>
-              <Stack direction='row' gap="20px" alignItems='center'>
+              <Stack direction="row" gap="20px" alignItems="center">
                 <Typography variant="body1" sx={{ opacity: 0.7 }}>
                   {parseInt(viewCount).toLocaleString()} views
                 </Typography>
@@ -58,10 +71,15 @@ const VideoDetail = () => {
             </Stack>
           </Box>
         </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
       </Stack>
-      <Box px={2} py={{ md: 1, xs: 5}} justifyContent='center' alignItems='center'>
-        <Videos videos={videos} direction='column'/>
-      </Box>
     </Box>
   );
 };
