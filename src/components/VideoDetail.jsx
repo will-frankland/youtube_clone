b24f7a365687e478b15ac6b676db0f410bda8cd9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-13 14:25:44 -0500

    add initial styling Boxes and Stack and wrap React Player component inside - minor styling added

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -10,9 +10,15 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   return (
-    <div>
-      VideoDetail
-    </div>
+    <Box minHeight='95vh'>
+      <Stack direction={{ xs: 'column', md: 'row '}}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
